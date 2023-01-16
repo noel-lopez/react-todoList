58574d29975c9e0dfa10bac1e5d7a4b3ea629bcd
Noel Lopez <nlopez.publi@gmail.com>
2023-01-16 10:01:58 +0100

    refactor: abstract card component onClick functions and emojis text

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -11,6 +11,16 @@ const CardComponent = ({
   goDown
 }) => {
   // TODO: use svg icons instead of emojis
+
+  const onPendingButtonClick = () => changeStatus("pending");
+  const onInProgressButtonClick = () => changeStatus("in progress");
+  const onDoneButtonClick = () => changeStatus("done");
+  const pendingEmoji = getEmoji("pending");
+  const inProgressEmoji = getEmoji("in progress");
+  const doneEmoji = getEmoji("done");
+  const deleteEmoji = getEmoji("delete");
+  const goToTaskEmoji = getEmoji("goToTask");
+
   return (
     <div className="card">
       <h3>{getEmoji(task.status)} {task.title}</h3>
@@ -18,11 +28,11 @@ const CardComponent = ({
       {/* <button onClick={() => logTask(task)}>log</button> */}
       <button onClick={goUp}>⬆</button>
       <button onClick={goDown}>⬇</button>
-      {task.status !== 'pending' && (<button onClick={() => changeStatus("pending")}>{getEmoji("pending")}</button>)}
-      {task.status !== 'in progress' && (<button onClick={() => changeStatus("in progress")}> {getEmoji("in progress")} </button>)}
-      {task.status !== 'done' && (<button onClick={() => changeStatus("done")}> {getEmoji("done")} </button>)}
-      <button onClick={deleteTask}> {getEmoji("delete")} </button>
-      <button onClick={goToTask}> {getEmoji("goToTask")} </button>
+      {task.status !== 'pending' && (<button onClick={onPendingButtonClick}> {pendingEmoji} </button>)}
+      {task.status !== 'in progress' && (<button onClick={onInProgressButtonClick}> {inProgressEmoji} </button>)}
+      {task.status !== 'done' && (<button onClick={onDoneButtonClick}> {doneEmoji} </button>)}
+      <button onClick={deleteTask}> {deleteEmoji} </button>
+      <button onClick={goToTask}> {goToTaskEmoji} </button>
     </div>
   )
 }
